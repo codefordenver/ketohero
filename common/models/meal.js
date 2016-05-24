f5d1a9d71072a295949eea1a39bc20fe0e76aa62
Scott Smeester <scott@smeester.com>
2016-05-24 04:55:46 -0600

    getting something to work

--- common/models/meal.js
+++ common/models/meal.js
@@ -1,3 +1,29 @@
 module.exports = function(Meal) {
+  Meal.addMeal = function(userId, mealName, mealDesc, foods, cb) {
+    var response;
+    var newMeal = {
+      userId: userId, 
+      name: mealName, 
+      description: mealDesc, 
+      foods: foods}
 
+    var models = Meal.app.Models;
+
+    console.log('models', models);
+
+    Meal.create(newMeal, function(err, meal){
+      if (err) {
+        return cb(err);
+      }
+      cb(null, meal)
+    })
+  }
+  Meal.remoteMethod('addMeal', {
+    accepts: [
+      {arg: 'userId', type: 'number'},
+      {arg: 'name', type:'string'},
+      {arg: 'description', type: 'string'},
+      {arg: 'foods', type: 'array'}],
+    returns: {arg: 'addMeal', type: 'string'}
+  })
 };
